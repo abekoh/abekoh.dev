04f9b0b137bc3026acc28fcfc744ce80c2bc6b40
Kotaro Abe <abekoh.bcky@gmail.com>
2023-08-19 14:41:13 +0900

    test: add

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,22 @@
-import app from '.'
+import app from ".";
 
-describe('Test the application', () => {
-  it('Should return 200 response', async () => {
-    const res = await app.request('/')
-    expect(res.status).toBe(200)
-  })
-})
+describe("root", () => {
+  it("return 200", async () => {
+    const res = await app.request("/");
+    expect(res.status).toBe(200);
+  });
+  it("has mstdn.jp link", async () => {
+    const res = await app.request("/");
+    const body = await res.text();
+    expect(body).toMatch('rel="me" href="https://mstdn.jp/@abekoh"');
+  });
+});
+
+describe("atproto", () => {
+  it("return value", async () => {
+    const res = await app.request("/.well-known/atproto-did");
+    expect(res.status).toBe(200);
+    const body = await res.text();
+    expect(body).toEqual("did:plc:eadrwmh2en6uubkc5cmslqom");
+  });
+});
